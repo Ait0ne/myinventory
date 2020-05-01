cbb734a22e3a2392a57a31060729ca980eb36a92
Ait0ne <62454158+Ait0ne@users.noreply.github.com>
2020-05-01 15:16:40 +0300

    Update settings.py

--- Inventory/Inventory/settings.py
+++ Inventory/Inventory/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'Inventory/static/'
+STATIC_URL = '/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
